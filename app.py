24b20a9ec050a9c520a5ae5f65d9cc0f13123f71
florezal754 <florezal754@gmail.com>
2022-10-27 18:12:12 +0200

    upload GUI

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 from flask import Flask, render_template, request, flash
+import pandas as pd
+import json
+import plotly 
+import plotly.express as px 
+import plotly.io as pio
+
 
 app = Flask(__name__)
 app.secret_key = "manbearpig_MUDMAN888"
@@ -12,3 +18,24 @@ def index():
 def greeter():
 	flash("Hi " + str(request.form['name_input']) + ", great to see you!")
 	return render_template("index.html")
+
+Dataset = pd.read_csv('penalty_signals.csv', encoding="UTF-8")
+Signals = pd.read_csv('signals16_21.csv', encoding="UTF-8")
+
+@app.route("/data", methods=['POST', 'GET'])
+def post_data():
+    Data_jsonStr = Signals.to_json()
+    pythonObj = json.loads(Data_jsonStr)
+    #response = pythonObj
+    fig = px.line(Signals, x='datetime', y='APEF')
+    ploting_plotly = pio.show(fig)
+    return ploting_plotly
+
+@app.route("/signals")
+def signals():
+    return render_template('historical.html')
+
+
+    
+
+app.run()
