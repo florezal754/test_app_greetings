1f25c588873bbc94e7d107042ed70606cec1c0d6
LauraFlórez <59582182+florezal754@users.noreply.github.com>
2022-11-24 22:31:32 +0100

    Update archiv.py

--- archiv.py
+++ archiv.py
@@ -8,6 +8,7 @@ import plotly
 import plotly.graph_objects as go
 import plotly.express as px
 from plotly.subplots import make_subplots 
+import plotly.io as pio
 
 #methodology dependencies
 import requests 
